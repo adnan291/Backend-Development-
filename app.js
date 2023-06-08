39ae1699739ee1319f51cf6f7c85c9caa89c4803
adnan291 <mohd.adnan.627492@gmail.com>
2023-06-08 21:00:02 +0530

    Writing and reading from a file

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
     if(url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
-        res.write(`<body><form action="/message" method="POST">message1<input type="text" name="message"></input><button type="submit">Send</button></form></body>`);
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"></input><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
